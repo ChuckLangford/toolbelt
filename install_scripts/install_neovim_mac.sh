6b6a2d19b66ff1d88b0ed0cd83f8287dc64db3f8
John R. Hampton <johnrhampton@users.noreply.github.com>
2018-07-19 22:10:47 -0400

    automate steps to use .vimrc
    automate steps to use .vimrc

--- install_scripts/install_neovim_mac.sh
+++ install_scripts/install_neovim_mac.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
 brew install neovim
+
+# automate steps to use .vimrc
+mkdir -p ~/.config/nvim && cd $_ && touch init.vim
+printf "set runtimepath^=~/.vim runtimepath+=~/.vim/after\nlet &packpath = &runtimepath\nsource ~/.vimrc" >> init.vim
